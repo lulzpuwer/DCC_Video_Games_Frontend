ebc8a900ebcb2cee188bbf81c98ebc16332cd240
Austin <ofthevalley111@gmail.com>
2022-05-16 14:50:04 -0500

    created Component GamesTable + instantiated GamesTable on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import GameTable from './Components/GameTable/GameTable'
+import axios from 'axios';
 
 function App() {
+
+
+  const [games, setGames] = useState([])
+  
+  
+  useEffect(() => {
+    GetAllGames();
+  },[])
+
+
+  async function GetAllGames(){
+    let response = await axios.get("https://localhost:7260/api/games/");
+    console.log(response.data);
+    setGames(response.data)
+  }
+
   return (
-    <div className="App">
-      <p>Hello World</p>
+    <div >
+      <GameTable games ={games} />
+      
    
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/Components/GameTable/GameTable.jsx
@@ -0,0 +1,45 @@
+const GameTable = (props) => {
+  return (  
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Genre</th>
+            <th>platform</th>
+            <th>publisher</th>
+            <th>rank</th>
+            <th>year</th>
+            <th>North American Sales</th>
+            <th>Europe Sales</th>
+            <th>Japan Sales</th>
+            <th>Other Sales</th>
+            <th>Global Sales</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.games.map((game, index) => {
+            return(
+              <tr key={index}>
+              <td>{game.name}</td>
+              <td>{game.genre}</td>
+              <td>{game.platform}</td>
+              <td>{game.publisher}</td>
+              <td>{game.rank}</td>
+              <td>{game.year}</td>
+              <td>{game.northAmericanSales}</td>
+              <td>{game.europeSales}</td>
+              <td>{game.japanmSales}</td>
+              <td>{game.otherSales}</td>
+              <td>{game.globalSales}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      
+    </div>
+  );
+}
+ 
+export default GameTable;
